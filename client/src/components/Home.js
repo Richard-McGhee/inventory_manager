a4de051c4a1fcad1988932fb64985699d92132f6
Richard McGhee <richardjmcghee@yahoo.com>
2022-01-28 14:04:03 -0500

    Ran into major issues on my end, code should be fine but I need to fix something with my setup for future projects.

---
+++ client/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from "styled-components"
+
+const HomeStyles = styled.div`
+    h1{
+        color: #D3C422;
+    }
+`
+
+const Home = () => {
+    return ( 
+        <HomeStyles>
+            <h1>Placeholder</h1>
+        </HomeStyles>
+    )
+}
+ 
+export default Home

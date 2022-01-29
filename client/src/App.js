1ba0863476b131766f82529fbbc54bd6893eac53
Jeremy Boggs <nicocchilover@gmail.com>
2022-01-29 13:52:10 -0500

    downgraded packages to fix browser start and router issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react'
-import { Route, Link } from 'react-router-dom'
-import Home from './components/Home'
-import ItemForm from './components/ItemForm'
-import CompanyForm from './components/CompanyForm'
-import UserForm from './components/UserForm'
-import './App.css'
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import ItemForm from "./components/ItemForm";
+import CompanyForm from "./components/CompanyForm";
+import UserForm from "./components/UserForm";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/" className="navLinks">Home</Link>
-        <Link to="/addItem" className="navLinks">+</Link>
-        <Link to="/companyInventory" className="navLinks">Inventory</Link>
-        <Link to="/login" className="navLinks">Login</Link>
+        <Link to="/" className="navLinks">
+          Home
+        </Link>
+        <Link to="/addItem" className="navLinks">
+          +
+        </Link>
+        <Link to="/companyInventory" className="navLinks">
+          Inventory
+        </Link>
+        <Link to="/login" className="navLinks">
+          Login
+        </Link>
       </nav>
-      <Route exact path="/" component={Home} />
-      <Route path="/addItem" component={ItemForm} />
-      <Route path="/companyInventory" component={CompanyForm} />
-      <Route path="/login" component={UserForm} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/addItem" component={ItemForm} />
+        <Route path="/companyInventory" component={CompanyForm} />
+        <Route path="/login" component={UserForm} />
+      </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 const HomeStyles = styled.div`
-    h1{
-        color: #D3C422;
-    }
-`
+  h1 {
+    color: #d3c422;
+  }
+`;
 
 const Home = () => {
-    return ( 
-        <HomeStyles>
-            <h1>Placeholder</h1>
-        </HomeStyles>
-    )
-}
- 
-export default Home
+  return (
+    <HomeStyles>
+      <h1>Placeholder</h1>
+      <h3>Placeholder2</h3>
+    </HomeStyles>
+  );
+};
+
+export default Home;
